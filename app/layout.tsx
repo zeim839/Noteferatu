6fabe76f149b6724af02f29822b41f48ad7f264c
Daniel Wildsmith <danielwildsmith@users.noreply.github.com>
2025-02-12 23:07:28 -0500

    feat: implement layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
+import { LayoutProvider, Layout, LayoutContent } from "@/components/ui/layout"
 import "./globals.css"
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-})
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-})
 
 export const metadata: Metadata = {
   title: "NoteFeratu",
@@ -24,10 +14,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body>
+        <LayoutProvider>
+          <Layout/>
+          <LayoutContent>
+            {children}
+          </LayoutContent>
+        </LayoutProvider>
       </body>
     </html>
   )
