0dd35c240c42f946879920e0b6171592afb4e6c4
zeim839 <zeim839@users.noreply.github.com>
2025-02-25 18:52:33 -0500

    refactor: chat component

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,9 @@
 "use client"
 
 import { useRouter } from "next/navigation"
-import ChatOverlay from "@/components/ui/chat"
-import { useState } from "react"
 
 export default function Home() {
   const router = useRouter()
-  const [isOpen, setIsOpen] = useState(false);
-  const [source, setSource] = useState("GPT");
-
-  const toggleChat = () => {
-    setIsOpen(!isOpen);
-  }
-
-  const handleSourceChange = (newSource: string) => {
-    setSource(newSource);
-    };
-
   return (
     <div className="w-full h-full flex items-center justify-center">
       <div className="flex gap-1 flex-col items-center justify-center"
@@ -24,13 +11,6 @@ export default function Home() {
         <div className="rounded-full w-12 h-12 bg-[#4C8EDA]" />
         <p>Click me!</p>
       </div>
-      {/* Chat Overlay */}
-      <ChatOverlay
-        isOpen={isOpen}
-        source={source}
-        onClose={toggleChat}
-        onSourceChange={handleSourceChange}
-      />
     </div>
   )
 }
