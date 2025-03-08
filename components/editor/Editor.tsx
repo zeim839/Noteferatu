9c7ba0128f7ee7917786724f3c6194c0a660dc34
Daniel Wildsmith <danielwildsmith@users.noreply.github.com>
2025-03-08 18:18:12 -0500

    keep sidebars transparent while note content has solid background

--- components/editor/Editor.tsx
+++ components/editor/Editor.tsx
@@ -9,10 +9,12 @@ import { defaultKeymap, indentWithTab } from "@codemirror/commands"
 import { syntaxHighlighting } from "@codemirror/language"
 import { markdownHighlightStyle, codeMirrorTheme } from "./theme"
 import Decorations from "./Decorations"
+import { useEditorBackground } from "./background"
 
 export default function Editor() {
   const [/*text*/, setText] = useState<string>('')
   const editorRef = useRef(null)
+  const { setEditorMode } = useEditorBackground();
   const onUpdate = useMemo(() =>
     EditorView.updateListener.of((v) => {
       setText(v.state.doc.toString())
@@ -20,8 +22,8 @@ export default function Editor() {
 
   useEffect(() => {
     if (!editorRef.current) return
-    document.body.style.backgroundColor = '#FBF9F3'
-        const state = EditorState.create({
+    setEditorMode(true);
+    const state = EditorState.create({
       doc: '# Heading 1\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n## Heading 2\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n### Heading 3\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       extensions: [
         basicSetup,
@@ -40,8 +42,11 @@ export default function Editor() {
       parent: editorRef.current,
     });
 
-    return () => view.destroy()
-  }, [onUpdate])
+    return () => {
+      view.destroy()
+      setEditorMode(false)
+    }
+  }, [onUpdate, setEditorMode])
 
   return (
     <div className='h-[calc(100vh-80px)] overflow-hidden relative max-w-[800px] w-full m-auto'>
