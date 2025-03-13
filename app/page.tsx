45eb40e3dc84727d7447f9fff6080093e3fe1157
Daniel Wildsmith <danielwildsmith@users.noreply.github.com>
2025-03-13 15:15:05 -0400

    feat: implement DatabaseProvider with graph

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,8 @@
 "use client"
 
-import { useEffect } from "react"
 import Graph from "@/components/graph"
-import { appLocalDataDir } from '@tauri-apps/api/path'
-import NoteController from "@/lib/controller/NoteController"
-import path from "path"
 
 export default function Home() {
-  useEffect(() => {
-    appLocalDataDir().then(async (dir: string) => {
-      const notes = new NoteController(path.join(dir, 'db.sqlite'))
-      console.log(await notes.readAll())
-    })
-  }, [])
   return (
     <div className="w-full h-full flex items-center justify-center">
       <Graph />
