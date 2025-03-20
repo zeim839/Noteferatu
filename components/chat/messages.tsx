c9586b7d10f7582ac5e54327e38a17ac71383502
zeim839 <zeim839@users.noreply.github.com>
2025-03-20 14:06:06 -0400

    doc: added comments

--- components/chat/messages.tsx
+++ components/chat/messages.tsx
@@ -1,5 +1,7 @@
 import { Message } from "@/lib/OpenRouter"
 
+// MessageField is a single message bubble. Its style depends on whether
+// the message is from user or system (LLM response).
 export const MessageField = (message: Message, index: number) => {
   const isUser = message.role === "user"
   return (
@@ -14,7 +16,10 @@ export const MessageField = (message: Message, index: number) => {
 
 type BottomRef = React.RefObject<HTMLDivElement | null>
 
+// MessageView contains all LLM chat messages.
 export const MessageView = (messages: Message[], isTyping: boolean, ref: BottomRef) => {
+
+  // Show a placeholder message whenever there are no messages.
   if (messages.length === 0) {
     return (
       <div className="flex flex-col justify-center items-center text-center text-gray-700">
@@ -29,7 +34,11 @@ export const MessageView = (messages: Message[], isTyping: boolean, ref: BottomR
   return (
     <div className="flex-1 overflow-auto flex flex-col gap-3"
       style={{maxHeight: 'calc(100vh - 120px)'}}>
+
+      { /* Render each message as a MessageField object. */ }
       {messages.map(MessageField)}
+
+      { /* Show a grayed-out 'typing' placeholder when loading */ }
       {isTyping && (
         <div className="self-start border border-[#979797] bg-[#F6F6F6] text-black rounded-md p-3 text-sm">
           <div className="">
