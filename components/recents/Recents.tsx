9242b09d33f7e3e83d651163482970241a512107
zeim839 <zeim839@users.noreply.github.com>
2025-03-20 14:04:59 -0400

    refactor: use DatabaseProvider instead of NoteController

--- components/recents/Recents.tsx
+++ components/recents/Recents.tsx
@@ -1,26 +1,14 @@
 import RecentsCard from "./RecentsCard"
-import React, { useState, useEffect, useMemo, useLayoutEffect} from "react"
-import { appLocalDataDir } from '@tauri-apps/api/path'
+import { useState, useEffect, useMemo, useLayoutEffect } from "react"
 import { Button } from "@/components/ui/button"
 import { PlusIcon } from "lucide-react"
 import { useRouter } from "next/navigation"
-import NoteController from "@/lib/controller/NoteController"
-import path from "path"
+import { Note } from "@/lib/controller/NoteController"
+import { useDB } from "@/components/DatabaseProvider"
+import { toast } from "sonner"
 
-type NoteData = {
-  id      : number
-  title   : string
-  content : string
-  atime   : number
-}
-
-async function getRecents(count: number): Promise<NoteData[] | null> {
-  const appDataDir = await appLocalDataDir()
-  const controller = new NoteController(path.join(appDataDir, 'db.sqlite'))
-  const notes = await controller.getRecents(count)
-  return notes as NoteData[]
-}
-
+// useWindowSize tracks the window dimensions. It is used to calculate
+// the number of RecentCards to show in the recents sidebar.
 function useWindowSize() {
   const [size, setSize] = useState([0, 0])
   useLayoutEffect(() => {
@@ -34,36 +22,48 @@ function useWindowSize() {
   return size
 }
 
+// Recents displays the most recently accessed notes. It only displays
+// enough notes to fill the window height.
 export default function Recents() {
   const [cardCount, setCardCount] = useState<number>(0)
-  const [recentsData, setRecentsData] = useState<NoteData[] | null>([])
+  const [recentsData, setRecentsData] = useState<Note[] | null>([])
   const [, height] = useWindowSize()
   const [isLoading, setIsLoading] = useState(true)
+
+  const db = useDB()
   const router = useRouter()
+
+  // Height of a RecentsCard container.
   const divHeight = 77
 
   useMemo(() => {
     setCardCount(Math.round((height - 60)/(divHeight+8)))
   }, [height])
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const data = await getRecents(cardCount)
-        setRecentsData(data)
-      } catch {
-        setRecentsData(null)
-      } finally {
-        setIsLoading(false)
+  // fetchData reads all notes.
+  const fetchData = async () => {
+    try { setRecentsData(await db.notes.readAll()) }
+    catch (error) {
+      let description = 'An unknown error has occurred'
+      if (error instanceof Error) {
+        description = error.message
       }
-    }
-    fetchData()
-  }, [cardCount])
+      toast('Error: Could Not Fetch Recent Notes', { description })
+      setRecentsData(null)
+    } finally { setIsLoading(false) }
+  }
 
+  useEffect(() => {
+    if (!db) return
+    fetchData()
+  }, [cardCount, db])
+
+  // Stall when notes are still loading.
   if (isLoading) {
     return null
   }
 
+  // When notes are loaded/available, render them to the DOM.
   if (recentsData && recentsData.length > 0) {
     const recentsCardsList = recentsData.slice(0, cardCount).map((note, i) => (
       <div key={i}
@@ -84,6 +84,7 @@ export default function Recents() {
     )
   }
 
+  // Show an error message if a non-empty array is returned.
   if (recentsData === null) {
     return (
       <div className="flex h-full items-center justify-center">
@@ -94,6 +95,8 @@ export default function Recents() {
     )
   }
 
+  // Show a placeholder asking the user to create a new note when there
+  // are no notes available.
   return (
     <div className="h-full flex flex-col justify-center items-center text-center text-gray-700">
       <div className='z-40'>
