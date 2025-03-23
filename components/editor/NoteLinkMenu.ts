867dd3b3f1155d6fa6b9c5d42ed346a99775d2d6
Daniel Wildsmith <danielwildsmith@users.noreply.github.com>
2025-03-23 19:27:06 -0400

    feat: link menu displays matching note options

--- components/editor/NoteLinkMenu.ts
+++ components/editor/NoteLinkMenu.ts
@@ -1,18 +1,36 @@
 import { CompletionContext } from "@codemirror/autocomplete"
+import { Note } from "@/lib/controller/NoteController"
 
-export const NoteLinkMenu = (context: CompletionContext) => {
-    const before = context.matchBefore(/\[[^\]]*\]\(node:/)
+export const NoteLinkMenu = (notes: Note[]) => (context: CompletionContext) => {
+    const before = context.matchBefore(/\[[^\]]*\]\(node:([^)]*)/)
+    if (!before) return null
 
-    if (before && before.text.endsWith("(node:")) {
-      return {
-        from: before.from + before.text.length,
-        options: [
-          { label: "Option 1" },
-          { label: "Option 2" },
-          { label: "Option 3" } // label: title, apply: id
-        ]
-      }
+    const query = before.text.match(/\[[^\]]*\]\(node:(.*)$/)?.[1] || ""
+    const from = before.from + before.text.length - query.length
+    const sortedNotes = [...notes].sort((a, b) => b.mtime - a.mtime)
+    let filteredNotes: Note[]
+    if (query.length === 0) {
+        filteredNotes = sortedNotes.slice(0, 3)
+    } else {
+        filteredNotes = sortedNotes.filter(note =>
+          note.title.toLowerCase().startsWith(query.toLowerCase())
+        )
     }
 
-    return null
+    if (filteredNotes.length === 0) {
+        return {
+          from,
+          options: [{ label: "No Results Found", apply: "-1" }],
+          filter: false
+        }
+    }
+    const options = filteredNotes.map(note => ({
+      label: note.title,
+      apply: `${note.id}`
+    }))
+    return {
+      from,
+      options,
+      filter: false
+    }
 }
