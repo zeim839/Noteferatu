4fd05b286c9a3c6c219516637ca6efddeeee17ce
malangadan <malangadan@ufl.edu>
2025-03-26 09:58:51 -0400

    feat: render search results in CommandMenu

--- components/navigation/LeftNavigation.tsx
+++ components/navigation/LeftNavigation.tsx
@@ -8,6 +8,7 @@ import { Button } from "@/components/ui/button"
 import Recents from "@/components/recents/Recents"
 import { NavigationState } from "./NavigationState"
 import { cn } from "@/lib/utils"
+import { useDB } from "@/components/DatabaseProvider"
 
 import {
   Command,
@@ -21,11 +22,13 @@ import {
 // LeftNavigation consists of the 'recents' hamburger menu and search bar.
 const LeftNavigation = ({ state } : { state: NavigationState }) => {
   const [searchValue, setSearchValue] = useState<string>("")
+  const [searchResults, setSearchResults] = useState<Note[]>([])
   const [isOpen, setIsOpen] = useState<boolean>(false)
   const commandRef = useRef<HTMLDivElement>(null)
   const inputRef = useRef<HTMLInputElement>(null)
   const isNotePage = usePathname() === '/note'
   const router = useRouter()
+  const db = useDB()
 
   // Pressing the meta/ctrl + K keybinding opens the Command menu.
   // It automatically focuses on the CommandInput.
@@ -36,6 +39,22 @@ const LeftNavigation = ({ state } : { state: NavigationState }) => {
       setTimeout(() => inputRef.current?.focus(), 0)
     }
   }
+
+  // Performs Note search
+  const noteSearch = async (searchValue: string) => {
+    // If search is only whitespace clear the search results
+    if (!searchValue.trim()){
+      setSearchResults([])
+      return
+    }
+    const results = await db.notes.search(searchValue)
+    setSearchResults(results)
+  }
+
+  useEffect (() => {
+    if (!db) return
+    noteSearch(searchValue)
+  }, [searchValue])
 
   useEffect(() => {
     document.addEventListener('keydown', handleKeyDown)
@@ -96,7 +115,22 @@ const LeftNavigation = ({ state } : { state: NavigationState }) => {
           ref={inputRef}
         />
         <CommandList className={cn(!isOpen && "hidden")}>
-          <CommandEmpty>No results found.</CommandEmpty>
+        {/* Condition ensures empty message only renders if notes is empty */}
+        {searchResults.length === 0 && (<CommandEmpty>No results found</CommandEmpty>)}
+          {/* Condition ensures Notes group only renders when there is a note searched */}
+          <CommandGroup heading="Notes" className={cn(searchResults.length !== 0 ? "block" : "hidden")}>
+            {searchResults.map((note) => (
+              <CommandItem
+                key={note.id}
+                value={`${note.title} ${note.snippetContent}`}
+                onSelect={() => handleSelect(() => router.push(`/note?id=${note.id}`))}>
+                <div className="flex flex-col">
+                  <span className="font-medium">{note.title}</span>
+                  <span className="text-xs text-muted-foreground">{note.snippetContent}</span>
+                </div>
+              </CommandItem>
+            ))}
+          </CommandGroup>
           <CommandGroup heading="Suggestions">
             <CommandItem
               onSelect={() => handleSelect(() => state.setLeftOpen(!state.isLeftOpen))}>
