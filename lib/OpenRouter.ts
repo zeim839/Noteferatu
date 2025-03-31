cc12275712d59858c0cd1f73ea2b5f766f916619
AntonCSalvador <antoncsalvador@gmail.com>
2025-03-31 10:39:43 -0400

    feat: integrate Settings with keys database

--- lib/OpenRouter.ts
+++ lib/OpenRouter.ts
@@ -11,48 +11,42 @@ export type Message = {
 // completion's stream.
 export type StreamFn = (chunk: string, index: number) => void
 
-// Use the OpenAI library to create an API client. Requests are sent to
-// OpenRouter, we're just using the OpenAI API schema.
-const client = new OpenAI({
-  baseURL: "https://openrouter.ai/api/v1",
-  apiKey: process.env.NEXT_PUBLIC_OPENROUTER_API_KEY || '',
-  dangerouslyAllowBrowser: true,
-})
-
 // systemPrompt is the first message prepended to a chat completions
 // request.
 const systemPrompt = "You are NoteFeratu, a note-taking app with a built-in helpful AI assistant"
 
 // Chat sends a chat completions API request and returns response.
-export const Chat = async (messages: Message[], model?: string) => {
-  const completion = await client.chat.completions.create({
-    model: model || 'deepseek/deepseek-r1:free',
-    messages: [
-      { role: 'system', content: systemPrompt },
-      ...messages
-    ],
-  })
-  return completion.choices[0]?.message?.content || ""
-}
+export const Chat = async (client: OpenAI, messages: Message[],
+  model?: string) => {
+    const completion = await client.chat.completions.create({
+      model: model || 'deepseek/deepseek-r1:free',
+      messages: [
+        { role: 'system', content: systemPrompt },
+        ...messages
+      ],
+    })
+    return completion.choices[0]?.message?.content || ""
+  }
 
 // Stream sends a chat completions API request and streams the token
 // responses. cb consumes the tokens as they arrive.
-export const Stream = async (messages: Message[], model?: string, cb?: StreamFn) => {
-  const stream = await client.chat.completions.create({
-    model: model || 'deepseek/deepseek-r1:free',
-    messages: [
-      { role: 'system', content: systemPrompt },
-      ...messages
-    ],
-    stream: true
-  })
-  if (typeof cb === 'undefined') {
+export const Stream = async (client: OpenAI, messages: Message[],
+  model?: string, cb?: StreamFn) => {
+    const stream = await client.chat.completions.create({
+      model: model || 'deepseek/deepseek-r1:free',
+      messages: [
+        { role: 'system', content: systemPrompt },
+        ...messages
+      ],
+      stream: true
+    })
+    if (typeof cb === 'undefined') {
+      return stream
+    }
+    let i : number = 0
+    for await (const chunk of stream) {
+      cb(chunk.choices[0]?.delta?.content || '', i)
+      i += 1
+    }
     return stream
   }
-  let i : number = 0
-  for await (const chunk of stream) {
-    cb(chunk.choices[0]?.delta?.content || '', i)
-    i += 1
-  }
-  return stream
-}
