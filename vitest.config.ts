9c9ad398df8bfda39c0e27bba072f0ae13ddd7b1
malangadan <malangadan@ufl.edu>
2025-04-17 04:47:05 -0400

    feat: datbase tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
@@ -8,5 +9,10 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './test/setup.ts',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
+    },
   },
 })
