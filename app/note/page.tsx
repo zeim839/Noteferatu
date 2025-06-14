4eedda362ee2b740b803cab2bf439bc130394e64
zeim839 <zeim839@users.noreply.github.com>
2025-06-14 14:55:33 +0300

    refactor: initialize project for V1

--- app/note/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-
-import Editor from "@/components/editor/Editor"
-
-export default function Page() {
-  return <Editor />
-}
--- components/navigation/NavigationState.tsx
+++
@@ -1,8 +0,0 @@
-// NavigationState conveniently exposes the state of the Navigation
-// component to LeftNavigation and RightNavigation.
-export type NavigationState = {
-  isLeftOpen   : boolean
-  isRightOpen  : boolean
-  setLeftOpen  : (open: boolean) => void
-  setRightOpen : (open: boolean) => void
-}
