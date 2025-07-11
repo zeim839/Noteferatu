a412d9bb8a7547448b5d7581ada00043f558acde
zeim839 <zeim839@users.noreply.github.com>
2025-07-11 13:29:39 +0300

    fix: move toolbar when entering fullscreen

--- components/window/titlebar.tsx
+++ components/window/titlebar.tsx
@@ -1,12 +1,28 @@
+import { getCurrentWindow } from "@tauri-apps/api/window"
 import { ComponentProps } from "react"
 import { cn } from "@/lib/utils"
+import { useState, useEffect } from "react"
 
 // Titlebar replaces the default OS titlebar.
-function Titlebar({ children, className, ...props }:
-ComponentProps<"div">) {
+function Titlebar({ children, className, ...props }: ComponentProps<"div">) {
+  const [isFullscreen, setIsFullscreen] = useState<boolean>(false)
+  useEffect(() => {
+    let unlisten: (() => void) | undefined
+    const spawnListener = async () => {
+      const window = getCurrentWindow()
+      setIsFullscreen(await window.isFullscreen())
+      unlisten = await window.listen('tauri://resize', async () =>{
+        setIsFullscreen(await window.isFullscreen())
+      })
+    }
+    spawnListener()
+    return () => { if (unlisten) {unlisten()} }
+  }, [])
   return (
     <div data-tauri-drag-region className={cn("bg-[#DCE0E8] h-[35px] flex items-center", className)} {...props} >
-      <div data-tauri-drag-region className="ml-[70px] px-[2px] pr-[5px] h-full w-full flex items-center justify-between z-[1000]">
+      <div data-tauri-drag-region data-is-fullscreen={isFullscreen}
+        className="ml-[5px] data-[is-fullscreen=false]:ml-[70px] px-[2px] pr-[5px] h-full w-full flex items-center justify-between z-[1000]"
+      >
         { children }
       </div>
     </div>
