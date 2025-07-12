368e90d48530de5de3619bcbbdf8d6728635641a
zeim839 <zeim839@users.noreply.github.com>
2025-07-12 17:15:26 +0300

    feat: implement model selection

--- components/agent/agentpanel.tsx
+++ components/agent/agentpanel.tsx
@@ -3,24 +3,23 @@
 import * as React from "react"
 import { Sidebar } from "@/components/window/sidebar"
 import { Button } from "@/components/core/button"
-
-import {
-  ChevronDownIcon,
-  ChevronRightIcon,
-  SlidersHorizontalIcon,
-  SendHorizontalIcon,
-  PaperclipIcon,
-  FileTextIcon,
-  PlusIcon,
-  Edit2Icon,
-  Trash2Icon,
-} from "lucide-react"
+import { Combobox } from "@/components/core/combobox"
+import * as Conversation from "./conversation"
 
 import {
   Tooltip,
   TooltipContent,
-  TooltipTrigger,
+  TooltipTrigger
 } from '@/components/core/tooltip'
+
+import {
+  ChevronDownIcon,
+  ChevronRightIcon,
+  ChevronsUpDownIcon,
+  SlidersHorizontalIcon,
+  SendHorizontalIcon,
+  PlusIcon,
+} from "lucide-react"
 
 const tokenStr = (tokens: number) => {
   return (tokens < 1000) ? tokens.toString() :
@@ -29,93 +28,11 @@ const tokenStr = (tokens: number) => {
       .toString() + 'k'
 }
 
-const timeAgo = (timestamp: number): string => {
-  const currentTimeStampSeconds = Math.floor(Date.now()/1000)
-  const diffInSeconds = Math.floor((currentTimeStampSeconds - timestamp))
-  if (diffInSeconds < 60) return `${diffInSeconds}s ago`
-  const diffInMinutes = Math.floor(diffInSeconds / 60)
-  if (diffInMinutes < 60) return `${diffInMinutes}m ago`
-  const diffInHours = Math.floor(diffInMinutes / 60)
-  if (diffInHours < 24) return `${diffInHours}h ago`
-  const diffInDays = Math.floor(diffInHours / 24)
-  if (diffInDays < 30) return `${diffInDays}d ago`
-  const diffInMonths = Math.floor(diffInDays / 30)
-  if (diffInMonths < 12) return `${diffInMonths}mo ago`
-  const diffInYears = Math.floor(diffInMonths / 12)
-  return `${diffInYears}y ago`
-}
-
-type Conversation = {
-  id: String
-  name: String
-  createdAt: number
-}
-
-const ConversationsEntry = ({ id, name, createdAt } : Conversation) => {
-  const [isHovered, setIsHovered] = React.useState<boolean>(false)
-  return (
-    <div
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-      className="flex flex-row justify-between w-full p-1.5 px-2 rounded-md hover:bg-[#DCE0E8]"
-    >
-      <p className="text-xs text-nowrap text-ellipsis overflow-x-hidden overflow-y-hidden">
-        {name}
-      </p>
-      <p
-        data-is-hovered={isHovered}
-        className="text-xs min-w-15 text-right data-[is-hovered=true]:hidden"
-      >
-        {timeAgo(createdAt)}
-      </p>
-      <div
-        className="hidden data-[is-hovered=true]:flex gap-2 min-w-15 justify-end"
-        data-is-hovered={isHovered}
-      >
-        <Button variant="ghost" size="icon" className="p-0 m-0 size-4">
-          <Edit2Icon strokeWidth={1} className="max-h-3 max-w-3"/>
-        </Button>
-        <Button variant="ghost" size="icon" className="p-0 m-0 size-4">
-          <Trash2Icon strokeWidth={1} className="max-h-3 max-w-3" />
-        </Button>
-      </div>
-    </div>
-  )
-}
-
-type ConversationsBodyProps = {
-  visible: boolean
-  conversations: Conversation[]
-}
-
-const ConversationsBody = ({ visible, conversations }: ConversationsBodyProps) => {
-  if (conversations.length == 0) {
-    return (
-      <div className="h-full w-full flex justify-center items-center pb-16">
-        <p className="text-xs">No conversations yet</p>
-      </div>
-    )
-  }
-
-  return (
-    <div
-      data-is-visible={visible}
-      className="data-[is-visible=false]:hidden mt-1"
-    >
-      {
-        conversations.map((item, index) =>
-          <ConversationsEntry key={index} id={item.id} name={item.name} createdAt={item.createdAt} />
-        )
-      }
-    </div>
-  )
-}
-
 function AgentPanel() {
   const [expandConvs, setExpandConvs] = React.useState<boolean>(false)
   const [usedTokens, setUsedTokens] = React.useState<number>(0)
   const [totalTokens, setTotalTokens] = React.useState<number>(200000)
-  const [conversations, setConversations] = React.useState<Conversation[]>([
+  const [conversations, setConversations] = React.useState<Conversation.Conversation[]>([
     {id: "0", name: "Software Engineering Assistance Request (need help)", createdAt: 1752250136},
     {id: "1", name: "Software Engineering Help", createdAt: 1752240136},
     {id: "2", name: "Another Example Conversation", createdAt: 1752230136},
@@ -152,48 +69,59 @@ function AgentPanel() {
         className="absolute top-[30px] w-full h-0 bg-[#E5E9EF] hidden data-[is-expanded=true]:block data-[is-expanded=true]:h-[calc(100vh-35px-30px)] z-1"
         data-is-expanded={expandConvs}
       >
-        <ConversationsBody
-          conversations={conversations}
-          visible={expandConvs}
-        />
+        <Conversation.Body body={conversations} />
       </div>
-      <div className="w-full h-[150px] bg-[#E5E9EF] outline outline-[#AEB3C0] grid grid-rows-[auto_32px]">
-        <div className="bg-[#EDF0F4] p-2 flex flex-col justify-between">
-          <div className="flex-1 relative">
-            <textarea
-              placeholder="Ask anything..."
-              className="w-full h-full min-h-[60px] p-2 text-sm resize-none bg-none focus:outline-none"
-            />
-          </div>
-          <div className="flex flex-row items-center justify-end gap-2">
+      <div className="w-full h-[150px] bg-[#EDF0F4] outline outline-[#AEB3C0] p-2 flex flex-col justify-between">
+        <div className="flex-1 relative">
+          <textarea
+            placeholder="Ask anything..."
+            className="w-full h-full min-h-[60px] p-2 text-sm resize-none bg-none focus:outline-none"
+          />
+        </div>
+        <div className="flex flex-row items-center justify-between">
+          <Combobox>
+            <Combobox.Trigger>
+              <Button
+                variant="outline"
+                tooltip="Select Model"
+                className="p-2 text-xs rounded-sm h-6 px-1 flex items-center justify-between"
+              >
+                No Model Selected
+                <ChevronsUpDownIcon strokeWidth={1.6}/>
+              </Button>
+            </Combobox.Trigger>
+            <Combobox.Values values={[]} />
+            <Combobox.EmptyBody className="p-4">
+              <div className="flex flex-col items-center gap-2.5">
+                <p className="text-sm text-center">
+                  No models available. Please configure a LLM provider.
+                </p>
+                <Button size="sm">
+                  Open Agent Settings
+                </Button>
+              </div>
+            </Combobox.EmptyBody>
+          </Combobox>
+          <div className="flex items-center gap-1">
             <Tooltip>
               <TooltipTrigger asChild>
-                <p className="text-xs max-h-[15px]">
-                  {tokenStr(usedTokens)}/{tokenStr(totalTokens)}
-                </p>
+                <div className="h-6 px-1 flex items-center">
+                  <p className="text-xs max-h-[15px]">
+                    {tokenStr(usedTokens)}/{tokenStr(totalTokens)}
+                  </p>
+                </div>
               </TooltipTrigger>
               <TooltipContent>
                 <p>Estimated Token Count</p>
               </TooltipContent>
             </Tooltip>
             <Button
-              variant="ghost"
+              variant="outline"
               size="icon"
-              className="w-[20px] h-[20px]"
               tooltip="Send Message"
             >
               <SendHorizontalIcon strokeWidth={1.6} />
             </Button>
-          </div>
-        </div>
-        <div className="w-full bg-[#E5E9EF] outline outline-[#AEB3C0] flex flex-row">
-          <div className="w-[36px] h-full outline outline-[#AEB3C0] flex items-center justify-center px-1 hover:bg-[#DCE0E8]">
-            <PaperclipIcon strokeWidth={1.6} className="h-[17px]"/>
-            <PlusIcon strokeWidth={1.6} className="w-[14px]" />
-          </div>
-          <div className="h-full w-[120px] bg-[#EDF0F4] outline outline-[#AEB3C0] flex flex-row items-center">
-            <FileTextIcon strokeWidth={1.6} className="h-[14px]" />
-            <p className="text-xs text-light">CIS4301_hw2.md</p>
           </div>
         </div>
       </div>
--- components/core/button.tsx
+++ components/core/button.tsx
@@ -15,11 +15,13 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
+          "border text-black shadow-md hover:bg-[#D4D8E1]",
         destructive:
-          "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
+          "text-[#FF2400] shadow-xs",
+        confirmation:
+          "text-[#008000] shadow-xs",
         outline:
-          "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
+          "text-black rounded-sm hover:bg-[#D4D8E1]",
         secondary:
           "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
         ghost:
@@ -27,10 +29,10 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
-        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
-        icon: "w-[24px] h-[24px]",
+        default: "h-9 px-4 py-2",
+        sm: "h-8 rounded-sm gap-1.5 px-3 has-[>svg]:px-2.5",
+        lg: "h-10 rounded-sm px-6 has-[>svg]:px-4",
+        icon: "size-6",
       },
     },
     defaultVariants: {
