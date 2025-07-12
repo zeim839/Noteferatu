4f2bb3294b92641c062060a545b01c5574d9e32e
zeim839 <zeim839@users.noreply.github.com>
2025-07-12 17:14:24 +0300

    fix: increase tooltip contrast

--- components/core/tooltip.tsx
+++ components/core/tooltip.tsx
@@ -44,7 +44,7 @@ function TooltipContent({
         data-slot="tooltip-content"
         sideOffset={sideOffset}
         className={cn(
-          "bg-[#EFF1F5] outline outline-[#B6B9C6] text-[#4C4F69] animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-[1000] w-fit origin-(--radix-tooltip-content-transform-origin) rounded-sm px-3 py-1.5 text-sm text-balance",
+          "bg-[#EFF1F5] shadow-md border text-[#4C4F69] animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-[1000] w-fit origin-(--radix-tooltip-content-transform-origin) rounded-sm px-3 py-1.5 text-sm text-balance",
           className
         )}
         {...props}
