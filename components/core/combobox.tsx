e6aa15b63249d727cd5cfba3361e33103cb59479
zeim839 <zeim839@users.noreply.github.com>
2025-07-12 17:13:58 +0300

    feat: create combobox core component

---
+++ components/core/combobox.tsx
@@ -0,0 +1,110 @@
+"use client"
+
+import * as React from "react"
+import { Slot } from "@radix-ui/react-slot"
+
+import {
+  Command,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/core/command"
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/core/popover"
+
+function Combobox({children} : React.ComponentProps<"div">) {
+  const [open, setOpen] = React.useState(false)
+
+  // Deconstruct combobox subcomponents.
+  const slots = React.useMemo(() => {
+    let trigger: React.ReactNode | null = null
+    let values: React.ReactNode | null = null
+    let emptyBody: React.ReactNode | null = null
+    React.Children.forEach(children, child => {
+      if (!React.isValidElement(child)) return
+      if (child.type === Combobox.Trigger) {
+        trigger = child
+        return
+      }
+      if (child.type === Combobox.Values) {
+        if ((child.props as ComboboxValuesProps).values.length == 0) {
+          return
+        }
+        values = child
+        return
+      }
+      if (child.type === Combobox.EmptyBody) {
+        emptyBody = child
+        return
+      }
+    })
+    return { trigger, values, emptyBody }
+  }, [children])
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        { slots.trigger }
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        {
+          (slots.values !== null) ?
+            <Command>
+              <CommandInput placeholder="Search..." className="h-9" />
+              <CommandList>
+                <CommandGroup>
+                  { slots.values }
+                </CommandGroup>
+              </CommandList>
+            </Command> :
+            slots.emptyBody
+        }
+      </PopoverContent>
+    </Popover>
+  )
+}
+
+function ComboboxSlot({ ...props }: React.ComponentProps<"div">) {
+  return (<Slot {...props} />)
+}
+
+function ComboboxEmptyBody({ ...props }: React.ComponentProps<"div">) {
+  return (<Slot {...props} />)
+}
+
+export type ComboboxValue = {
+  value: string,
+  label: string,
+}
+
+interface ComboboxValuesProps extends React.ComponentProps<"div"> {
+  values: ComboboxValue[]
+}
+
+function ComboboxValues({ values, ...props }: ComboboxValuesProps) {
+  return (values.length > 0) ?
+    <div {...props}>
+      {
+        values.map((value) => (
+          <CommandItem
+            key={value.value}
+            value={value.value}
+          >
+            {value.label}
+          </CommandItem>
+        ))
+      }
+    </div> : null
+}
+
+// Combobox Subcomponents.
+Combobox.Trigger = ComboboxSlot
+Combobox.EmptyBody = ComboboxEmptyBody
+Combobox.Values = ComboboxValues
+
+export { Combobox }
