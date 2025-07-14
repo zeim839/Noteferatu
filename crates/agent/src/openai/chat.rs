b1ed5ffdc2311dddeaf99e43237b3ece063107d5
zeim839 <zeim839@users.noreply.github.com>
2025-07-14 19:57:01 +0300

    fix: unintentional docs hyperlink

--- crates/agent/src/openai/chat.rs
+++ crates/agent/src/openai/chat.rs
@@ -315,7 +315,7 @@ pub enum ServiceTier {
 pub struct StreamOptions {
 
     /// If set, an additional chunk will be streamed before the data:
-    /// [DONE] message. The usage field on this chunk shows the token
+    /// `[DONE]` message. The usage field on this chunk shows the token
     /// usage statistics for the entire request, and the choices field
     /// will always be an empty array.
     ///
