75c7b58fac20b10758d81f6d7f0cd3ece7554808
zeim839 <zeim839@users.noreply.github.com>
2025-07-22 20:32:44 +0300

    feat: create input

---
+++ components/core/input.tsx
@@ -0,0 +1,20 @@
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+
+function Input({ className, type, ...props }: React.ComponentProps<"input">) {
+  return (
+    <input
+      type={type}
+      data-slot="input"
+      className={cn(
+        "file:text-black placeholder:text-[#737373] flex h-8 w-full min-w-0 rounded-sm border bg-[#EFF1F5] px-3 py-1 text-black shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+export { Input }
