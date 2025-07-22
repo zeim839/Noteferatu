4d35a91740188ca26aa3bcf9e9e591575ab16a94
zeim839 <zeim839@users.noreply.github.com>
2025-07-22 20:32:54 +0300

    feat: create link

---
+++ components/core/link.tsx
@@ -0,0 +1,25 @@
+import * as React from "react"
+import { cn } from "@/lib/utils"
+import { ExternalLinkIcon } from "lucide-react"
+import { openUrl } from "@tauri-apps/plugin-opener"
+
+interface LinkProps extends React.ComponentProps<"div"> {
+  href: string
+}
+
+function Link({ href, children, className }: LinkProps) {
+  return (
+    <a
+      onClick={() => openUrl(href)}
+      className={cn(
+        "inline-flex items-center w-fit gap-1 text-[#007AFF] hover:text-[#23B0FF]",
+        className,
+      )}
+    >
+      {children}
+      <ExternalLinkIcon className="w-3 mr-2" />
+    </a>
+  )
+}
+
+export { Link }
