d09df08e96376720dae25cf4a607db67898000f1
zeim839 <zeim839@users.noreply.github.com>
2025-11-03 19:04:28 +0400

    feat: oai_msg! macro

--- crates/macros/src/lib.rs
+++ crates/macros/src/lib.rs
@@ -1,4 +1,52 @@
 #[macro_export]
 macro_rules! oai_msg {
-    () => {}
+    ("developer", $first:expr $(, $rest:expr)*) => {{
+        let mut content = renfield::client::openai::Content::from($first);
+        $(
+            let other = renfield::client::openai::Content::from($rest);
+            content = content.combine(other);
+        )*;
+        renfield::client::openai::Message::Developer{ content }
+    }};
+    ("system", $first:expr $(, $rest:expr)*) => {{
+        let mut content = renfield::client::openai::Content::from($first);
+        $(
+            let other = renfield::client::openai::Content::from($rest);
+            content = content.combine(other);
+        )*;
+        renfield::client::openai::Message::System{ content }
+    }};
+    ("user", $first:expr $(, $rest:expr)*) => {{
+        let mut content = renfield::client::openai::Content::from($first);
+        $(
+            let other = renfield::client::openai::Content::from($rest);
+            content = content.combine(other);
+        )*;
+        renfield::client::openai::Message::User{ content }
+    }};
+    ("assistant", $first:expr $(, $rest:expr)*) => {{
+        let mut content = renfield::client::openai::Content::from($first);
+        $(
+            let other = renfield::client::openai::Content::from($rest);
+            content = content.combine(other);
+        )*;
+        renfield::client::openai::Message::Assistant{
+            audio: None,
+            content: Some(content),
+            refusal: None,
+            tool_calls: None,
+        }
+    }};
+    ("tool", $id:expr, $first:expr $(, $rest:expr)*) => {{
+        let id: String = String::from($id);
+        let mut content = renfield::client::openai::Content::from($first);
+        $(
+            let other = renfield::client::openai::Content::from($rest);
+            content = content.combine(other);
+        )*;
+        renfield::client::openai::Message::Tool{
+            tool_call_id: id,
+            content,
+        }
+    }};
 }
