05c48e7e9c67c2c461ed684fa8fc7d648a0b7fe0
zeim839 <zeim839@users.noreply.github.com>
2025-11-06 20:30:34 +0400

    refactor: move tools to separate module

---
+++ crates/renfield/src/client/ollama/tools.rs
@@ -0,0 +1,25 @@
+use serde::{Serialize, Deserialize};
+
+/// Tool call requested by the assistant.
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct ToolCall {
+
+    /// The function to call.
+    pub function: FunctionCall,
+}
+
+/// Function call requested by the assistant.
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct FunctionCall {
+
+    /// Name of the function to call.
+    pub name: String,
+
+    /// What the function does.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub description: Option<String>,
+
+    /// JSON object of arguments to pass to the function.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub arguments: Option<serde_json::Value>,
+}
